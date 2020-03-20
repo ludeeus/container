b7e8cf5d024edb0c0a94e15ca908d6398d9e1728
Joakim Sørensen <hi@ludeeus.dev>
2020-03-21 00:12:16 +0100

    New build setup (#5)
    * Test new build setup
    
    * actually do stuff
    
    * Add needs
    
    * Update builds
    
    * move stuff
    
    * Update structure
    
    * test
    
    * check?
    
    * Update base
    
    * better?
    
    * Cleanup
    
    * Add issue
    
    * Fix builds

---
+++ Scripts/Docker.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import subprocess
+
+REPO = "ludeeus/devcontainer"
+IMAGES = []
+
+REF = os.getenv("GITHUB_REF").split("/")[-1]
+EVENT = os.getenv("GITHUB_EVENT_NAME")
+
+def main(runtype):
+    if len(runtype) == 1:
+        print("Runtype is missing")
+        exit(1)
+
+    IMAGES.append(Image("base", "Base.dockerfile", []))
+    IMAGES.append(Image("base-debian", "BaseDebian.dockerfile", []))
+
+    IMAGES.append(Image("python", "Dockerfiles/Python.dockerfile", ["base"]))
+    IMAGES.append(Image("dotnet", "Dockerfiles/Dotnet.dockerfile", ["base-debian"]))
+    IMAGES.append(Image("netdaemon", "Dockerfiles/Netdaemon.dockerfile", ["dotnet", "base-debian"]))
+    IMAGES.append(Image("integration", "Dockerfiles/Integration.dockerfile", ["python"]))
+    IMAGES.append(Image("frontend", "Dockerfiles/Frontend.dockerfile", ["base"]))
+    IMAGES.append(Image("monster", "Dockerfiles/Monster.dockerfile", ["python", "integration"]))
+
+    if "build" in runtype:
+        build_all()
+    if "publish" in runtype:
+        publish_all()
+
+class Image:
+    def __init__(self, name, dockerfile, needs):
+        self.name = name
+        self.dockerfile = dockerfile
+        self.needs = needs
+        self.build = False
+        self.published = False
+
+    def build_image(self):
+        command = f"docker build --compress --no-cache -t {REPO}:{self.name} -f {self.dockerfile} ."
+        if self.name == "base":
+            command += f" -t {REPO}:latest"
+        run_command(command)
+        self.build = True
+
+    def publish_image(self):
+        if self.name == "base":
+            run_command(f'docker push {REPO}:latest')
+        run_command(f'docker push {REPO}:{self.name}')
+        if EVENT == "release":
+            run_command(f'docker push {REPO}:{self.name}-{REF}')
+        self.published = True
+
+def get_next(sortkey):
+    if sortkey == "build":
+        return sorted([x for x in IMAGES if not x.build], key=lambda x: x.needs, reverse=False)
+    return sorted([x for x in IMAGES if not x.published], key=lambda x: x.needs, reverse=False)
+
+def run_command(command):
+    build = subprocess.run([x for x in command.split(" ")])
+    if build.returncode != 0:
+        exit(1)
+
+def build_all():
+    while True:
+        image = get_next("build")
+        if not image:
+            break
+        image = image[0]
+        if [x for x in IMAGES if x.name in image.needs and not x.build]:
+            print("Build strategy is not correct")
+            exit(1)
+        image.build_image()
+
+
+def publish_all():
+    while True:
+        image = get_next("published")
+        if not image:
+            break
+        image = image[0]
+        image.publish_image()
+
+print(os.environ)
+main(sys.argv)
