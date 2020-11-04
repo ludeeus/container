239fb055ca404fbf2fe33c3fe107352dd8554ffe
Ludeeus <ludeeus@ludeeus.dev>
2020-11-04 20:56:23 +0000

    fix frontend

--- rootfs/common/usr/share/container/makefiles/frontend.mk
+++ rootfs/common/usr/share/container/makefiles/frontend.mk
@@ -1,5 +1,5 @@
 
-init: ## Init with custom-cards/boilerplate
+init: ## Initialize with custom-cards/boilerplate
 	source /workspaces/container/rootfs/common/usr/share/container/tools
 	git clone https://github.com/custom-cards/boilerplate-card.git /tmp/init
 	rm -R /tmp/init/.git
