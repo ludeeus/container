2caa675c817e38e64d6d8c81598febc6869a6160
Ludeeus <ludeeus@ludeeus.dev>
2020-12-24 12:39:13 +0000

    add build cleanup

---
+++ include/cleanup-build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -fr /include
+rm -fr /container
--- include/cleanup-debian.sh
+++ include/cleanup-debian.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-rm -fr /include
-rm -fr /container
 rm -fr /var/lib/apt/lists/*
 rm -fr /tmp/* /var/{cache,log}/*
