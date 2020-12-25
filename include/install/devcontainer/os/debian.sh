13f53d3ead107fde573f2623ca947e0a0dcac684
Ludeeus <ludeeus@ludeeus.dev>
2020-12-25 18:34:18 +0000

    fix devcontainer

--- include/install/devcontainer/os/debian.sh
+++ include/install/devcontainer/os/debian.sh
@@ -2,10 +2,10 @@
 
 echo -e "\\033[0;34mRunning install script 'devcontainer/os/debian.sh'\\033[0m"
 
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
-apt-get install -y --no-install-recommends \
-    nano \
-    openssh-client \
-    git \
-    shellcheck
+apt-get install -y --no-install-recommends shellcheck
+
+wget -q -O - https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/script-library/common-debian.sh | bash -
 
