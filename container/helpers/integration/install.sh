53e19aa8a6678e0f25b6c174f1f541663cfe3401
Ludeeus <ludeeus@ludeeus.dev>
2020-12-25 18:55:33 +0000

    update devcontainer examples

---
+++ container/helpers/integration/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# shellcheck source=/dev/null
+
+python3 -m pip --disable-pip-version-check install --upgrade git+git://github.com/home-assistant/home-assistant.git@dev
--- container/makefiles/integration.mk
+++ container/makefiles/integration.mk
@@ -1,2 +1,5 @@
 start:  ## Start Home Assistant with the integration loaded
 	@bash /opt/container/helpers/integration/start.sh
+
+install:  ## Install Home Assistant in the container
+	@bash /opt/container/helpers/integration/start.sh
