7845b88d34a101a3036b1dabf7f363c391609e9d
Ludeeus <ludeeus@ludeeus.dev>
2020-12-25 17:50:15 +0000

    limit build

--- scripts/should_build.sh
+++ scripts/should_build.sh
@@ -3,46 +3,48 @@
 container="$1"
 changed="$2"
 
-if [[ "$changed" =~ $container ]]; then
-    echo "true"
-    exit
-fi
-if [[ "$container" =~ base ]]; then
-    if [[ "$changed" =~ include/install/base- ]]; then
-        echo "true"
-        exit
+function shouldBuild () { echo "true" && exit;}
+
+if [[ "$changed" =~ $container ]]; then shouldBuild; fi
+if [[ "$changed" =~ include/cleanup ]]; then shouldBuild; fi
+
+if [[ "$container" =~ base/os ]]; then
+    if [[ "$changed" =~ base/Os.dockerfile ]]; then shouldBuild; fi
+
+    if [[ "$container" =~ alpine ]]; then
+        if [[ "$changed" =~ include/install/base/alpine.sh ]]; then shouldBuild; fi
+    elif [[ "$container" =~ debian ]]; then
+        if [[ "$changed" =~ include/install/base/debian.sh ]]; then shouldBuild; fi
     fi
 fi
+
+if [[ "$container" =~ base/runtime ]]; then
+    if [[ "$changed" =~ base/os ]]; then shouldBuild; fi
+    if [[ "$changed" =~ base/Runtime.dockerfile ]]; then shouldBuild; fi
+    if [[ "$container" =~ python ]]; then
+        if [[ "$changed" =~ include/install/python/common.sh ]]; then shouldBuild; fi
+        if [[ "$container" =~ alpine ]]; then
+            if [[ "$changed" =~ include/install/python/alpine.sh ]]; then shouldBuild; fi
+        elif [[ "$container" =~ debian ]]; then
+            if [[ "$changed" =~ include/install/python/debian.sh ]]; then shouldBuild; fi
+        fi
+    fi
+fi
+
+
 if [[ "$container" =~ -s6 ]]; then
-    if [[ "$changed" =~ include/install/s6.sh ]]; then
-        echo "true"
-        exit
-    fi
+    if [[ "$changed" =~ base/os ]]; then shouldBuild; fi
+    if [[ "$changed" =~ base/runtime ]]; then shouldBuild; fi
+    if [[ "$changed" =~ s6.dockerfile ]]; then shouldBuild; fi
+    if [[ "$changed" =~ include/install/s6.sh ]]; then shouldBuild; fi
 fi
+
+
 if [[ "$container" =~ devcontainer ]]; then
-    if [[ "$changed" =~ include/install/devcontainer- ]]; then
-        echo "true"
-        exit
-    elif [[ "$changed" =~ include/install/ghcli- ]]; then
-        echo "true"
-        exit
-    elif [[ "$changed" =~ include/install/container ]]; then
-        echo "true"
-        exit
-    fi
+    if [[ "$changed" =~ Devcontainer.dockerfile ]]; then shouldBuild; fi
+    if [[ "$changed" =~ include/install/container.sh ]]; then shouldBuild; fi
+    if [[ "$changed" =~ include/install/ghcli.sh ]]; then shouldBuild; fi
 fi
-if [[ "$container" =~ python ]]; then
-    if [[ "$changed" =~ include/install/python- ]]; then
-        echo "true"
-        exit
-    fi
-fi
-if [[ "$changed" =~ include/install/cleanup/ ]]; then
-    echo "true"
-    exit
-fi
-if [[ "$changed" =~ containerfiles/common ]]; then
-    echo "true"
-    exit
-fi
+
+
 echo "false"
