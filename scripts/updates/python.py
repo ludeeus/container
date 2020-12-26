e87ccfe0c252dad9e95157ca3ab8f53ebc7f2775
Ludeeus <ludeeus@ludeeus.dev>
2020-12-26 16:19:38 +0000

    Add update logic for Python

---
+++ scripts/updates/python.py
@@ -0,0 +1,46 @@
+"""Update python."""
+from typing import TYPE_CHECKING
+import requests
+
+from version import Version
+from jsonfile import JsonFile
+
+URL = "https://www.python.org/downloads"
+
+if TYPE_CHECKING:
+    from .version import Version
+    from .jsonfile import JsonFile
+
+install_python_version = JsonFile("./include/install/python/versions.json")
+alpine_python_config = JsonFile("./containerfiles/alpine/python/config.json")
+alpine_s6_python_config = JsonFile("./containerfiles/alpine-s6/python/config.json")
+debian_python_config = JsonFile("./containerfiles/debian/python/config.json")
+debian_s6_python_config = JsonFile("./containerfiles/debian-s6/python/config.json")
+devcontainer_integration = JsonFile(
+    "./containerfiles/devcontainer/integration/config.json"
+)
+devcontainer_python = JsonFile("./containerfiles/devcontainer/python/config.json")
+
+current = Version(install_python_version.read()["python"])
+
+request = requests.get(URL).text
+upstream = Version(request.split(">Download Python ")[2].split("<")[0])
+upstream = Version("3.9.2")
+
+# if current.equals(upstream):
+#    print(f"Nothing to do, both current and upstream is {current}")
+#    exit(0)
+
+install_python_version.update("python", upstream.string)
+alpine_python_config.update("tags", upstream.tags)
+alpine_s6_python_config.update("tags", upstream.tags)
+alpine_s6_python_config.update("args.BUILD_FROM_TAG", upstream.string)
+debian_python_config.update("tags", upstream.tags)
+debian_s6_python_config.update("tags", upstream.tags)
+debian_s6_python_config.update("args.BUILD_FROM_TAG", upstream.string)
+devcontainer_integration.update("args.BUILD_FROM_TAG", upstream.string)
+devcontainer_python.update("args.BUILD_FROM_TAG", upstream.string)
+devcontainer_python.update("tags", upstream.tags)
+
+with open("./commit", "w") as commit:
+    commit.write(f"Update Python from {current.string} to {upstream.string}")
