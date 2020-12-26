049efbacc79881b0b8285f2452d8dc0f0fca6635
Ludeeus <ludeeus@ludeeus.dev>
2020-12-26 13:19:43 +0000

    remove version

--- include/install/node/alpine.sh
+++ include/install/node/alpine.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 echo -e "\\033[0;34mRunning install script 'node/alpine.sh'\\033[0m"
-NODE_VERSION="$1"
 
 apk update
 apk add --no-cache \
@@ -9,4 +8,4 @@ apk add --no-cache \
     jq \
     curl
 
-bash /include/install/node/common.sh "$NODE_VERSION"
+bash /include/install/node/common.sh
--- include/install/node/debian.sh
+++ include/install/node/debian.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 echo -e "\\033[0;34mRunning install script 'node/debian.sh'\\033[0m"
-NODE_VERSION="$1"
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -11,5 +10,5 @@ apt-get -y install --no-install-recommends \
     jq \
     xz-utils
 
-bash /include/install/node/common.sh "$NODE_VERSION"
+bash /include/install/node/common.sh
 bash /include/cleanup/debian.sh
