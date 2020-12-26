077c84a1b02bb31d647af67aa59a25d3f56329fd
Ludeeus <ludeeus@ludeeus.dev>
2020-12-26 01:59:44 +0000

    fix ver

--- include/install/python/common.sh
+++ include/install/python/common.sh
@@ -2,25 +2,25 @@
 set -e
 echo -e "\\033[0;34mRunning install script 'python/common.sh'\\033[0m"
 
-PYTHON_VERSION="$1"
-PYTHON_INSTALL_PATH="/usr/local/python"
+VERSION=$(jq -r .ghcli /include/install/ghcli/versions.json)
+INSTALL_PATH="/usr/local/python"
 
-echo -e "\\033[0;34mInstalling Python ${PYTHON_VERSION} to ${PYTHON_INSTALL_PATH}\\033[0m"
+echo -e "\\033[0;34mInstalling Python ${VERSION} to ${INSTALL_PATH}\\033[0m"
 
-mkdir -p /tmp/python-src "${PYTHON_INSTALL_PATH}"
-curl -sSLf -o /tmp/python.tar.xz "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz"
+mkdir -p /tmp/python-src "${INSTALL_PATH}"
+curl -sSLf -o /tmp/python.tar.xz "https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tar.xz"
 tar -xJC "/tmp/python-src" --strip-components=1 -f /tmp/python.tar.xz
 
 cd /tmp/python-src || exit 1
 ./configure \
-    --prefix="${PYTHON_INSTALL_PATH}" \
+    --prefix="${INSTALL_PATH}" \
     --enable-optimizations \
     --with-ensurepip=install
 
 make -j"$(nproc)" EXTRA_CFLAGS="-DTHREAD_STACK_SIZE=0x100000"
 make install
 
-cd "${PYTHON_INSTALL_PATH}/bin" || exit 1
+cd "${INSTALL_PATH}/bin" || exit 1
 ln -s idle3 idle
 ln -s pydoc3 pydoc
 ln -s python3 python
