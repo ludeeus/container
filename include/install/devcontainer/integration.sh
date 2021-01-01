54875ee0c232a1697a4cab714991e0c4958c1f5a
Joakim Sørensen <hi@ludeeus.dev>
2021-01-01 14:57:27 +0100

    Fix pillow installation (#102)

--- include/install/devcontainer/integration.sh
+++ include/install/devcontainer/integration.sh
@@ -4,4 +4,12 @@ echo -e "\\033[0;34mRunning install script 'devcontainer/integration.sh'\\033[0m
 
 bash /include/install/devcontainer/python.sh
 
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y --no-install-recommends \
+    libjpeg-dev \
+    zlib1g-dev
+
+
 mkdir -p /config/custom_components
