ed0110c1eea3552d1a8dac3581a1919e5f3d8d4b
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2022-04-17 12:07:39 -0400

    Update container/helpers/common/homeassistant/set-version.sh
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>

--- container/helpers/common/homeassistant/set-version.sh
+++ container/helpers/common/homeassistant/set-version.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# shellcheck source=/dev/null
-
-source /opt/container/helpers/common/paths.sh
 
 read -p 'Set Home Assistant version: ' -r version
 python3 -m pip --disable-pip-version-check install --upgrade homeassistant=="$version"
