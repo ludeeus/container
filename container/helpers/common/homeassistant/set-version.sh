580a4c95f16e462664713bfc342fe9c6508b760c
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2022-04-17 13:09:45 -0400

    Allow users to run their own code as part of "Set Home Assistant Version" task. (#272)
    * Added postSetVerionHook.
    
    * Fix shellcheck error.
    
    * Update container/helpers/common/homeassistant/set-version.sh
    
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>
    
    * Update container/helpers/common/paths.sh
    
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>
    
    * Update container/helpers/common/homeassistant/set-version.sh
    
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>
    
    Co-authored-by: Joakim Sørensen <hi@ludeeus.dev>

--- container/helpers/common/homeassistant/set-version.sh
+++ container/helpers/common/homeassistant/set-version.sh
@@ -2,3 +2,7 @@
 
 read -p 'Set Home Assistant version: ' -r version
 python3 -m pip --disable-pip-version-check install --upgrade homeassistant=="$version"
+
+if [[ -n "$POST_SET_VERSION_HOOK" ]]; then
+    "$POST_SET_VERSION_HOOK" "$version"
+fi
