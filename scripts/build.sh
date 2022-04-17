77a748db1792e2a87ca4a671398172eebfae169d
Ludeeus <ludeeus@ludeeus.dev>
2022-04-17 11:58:30 +0000

    escape

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,7 +22,7 @@ while [[ $# -gt 0 ]]; do
             shift
             ;;
         --title)
-            buildCommand+=(" --label org.opencontainers.image.title="$2" ")
+            buildCommand+=(" --label org.opencontainers.image.title=\"$2\" ")
             shift
             ;;
         -t|--tag)
@@ -94,7 +94,7 @@ echo "${buildCommand[@]}"
 
 if [ "$test" != "true" ]; then
     # shellcheck disable=SC2145,SC2046
-    echo "docker build . --compress ${buildCommand[@]} --label "org.opencontainers.image.description="$(jq -c -r .description ./containerfiles/"$container"/config.json)"""
+    echo "docker build . --compress ${buildCommand[@]} --label "org.opencontainers.image.description=\"$(jq -c -r .description ./containerfiles/"$container"/config.json)\"""
     buildCommand+=("--platform ${platforms:-$(jq -r -c '.platforms | @csv' "./containerfiles/$container/config.json" | tr -d '"')}")
     set +e
     docker buildx rm builder
@@ -102,7 +102,7 @@ if [ "$test" != "true" ]; then
     docker buildx create --name builder --use
     docker buildx inspect --bootstrap
     # shellcheck disable=SC2068
-    docker buildx build . --compress ${buildCommand[@]} --label "org.opencontainers.image.description="$(jq -c -r .description ./containerfiles/"$container"/config.json)""
+    docker buildx build . --compress ${buildCommand[@]} --label "org.opencontainers.image.description=\"$(jq -c -r .description ./containerfiles/"$container"/config.json)\""
     docker buildx rm builder
 else
     # shellcheck disable=SC2145
